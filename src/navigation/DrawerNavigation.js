35acbbd1d2ac6e7c5dd3664e4d3e4ff72028cb16
IkramMahfoud <ikrammahfoud9nemo@gmail.com>
2024-02-05 12:28:39 +0100

    Correction : import de Ionicons

--- src/navigation/DrawerNavigation.js
+++ src/navigation/DrawerNavigation.js
@@ -1,9 +1,11 @@
 // DrawerNavigation.js
 import React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 import FAQ  from '../screens/FAQScreen';
 import BottomTabsNavigator from './BottomTabsNavigator';
+
 
 const Drawer = createDrawerNavigator();
 
@@ -19,6 +21,14 @@ const DrawerNavigation = () => {
         headerTitleStyle:{
           fontSize: 20,
         },
+        headerRight: () => (
+          <Ionicons
+            name="ios-settings"
+            size={24}
+            color="white"
+            style={{ marginRight: 16 }}
+          />
+        ),
       }}>
       <Drawer.Screen name="Accueil" component={BottomTabsNavigator} />
       <Drawer.Screen name="FAQ" component={FAQ} />
